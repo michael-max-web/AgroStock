ba43da31414a2ca70706408fd97c06258660eac4
michael-max-web <michael4671111@gmail.com>
2026-05-09 21:53:53 -0300

    ajustes de responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,48 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import "./App.css";
+
 import PaginaInicial from "./paginas/PaginaInicial/PaginaInicial";
 import ListaProdutos from "./paginas/ListaProdutos/ListaProdutos";
-import Rodape from "./componentes/Rodape/Rodape";
 import CadastroProduto from "./paginas/CadastroProdutos/CadastroProduto";
 
 import Cabecalho from "./componentes/Cabecalho/Cabecalho";
 
-
-
 const roteador = createBrowserRouter([
-    { 
+  {
     path: "",
     element: <PaginaInicial />,
   },
-   {
+  {
     path: "lista-produtos",
-    element: <ListaProdutos />
+    element: <ListaProdutos />,
   },
- {
-  path: "cadastro-produtos",
-  element: <CadastroProduto />
-},
-{
-  path: "cadastro-produtos/:id", // ROTA PARA EDIÇÃO
-  element: <CadastroProduto />
-},
+  {
+    path: "cadastro-produtos",
+    element: <CadastroProduto />,
+  },
+  {
+    path: "cadastro-produtos/:id",
+    element: <CadastroProduto />,
+  },
 ]);
 
-// App.jsx - Mantenha como está, ele é o seu "Layout Fixo"
 function App() {
   return (
-    <>
-      <Cabecalho /> {/* Este é o único cabeçalho do topo */}
-      <RouterProvider router={roteador} />
-      <ToastContainer position="top-center" autoClose={2000} />
-    </>
+    <div className="app-layout">
+      <Cabecalho />
+      <div className="app-conteudo">
+        <RouterProvider router={roteador} />
+      </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        theme="colored"
+      />
+    </div>
   );
 }
 
